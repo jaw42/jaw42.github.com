713a30a89c7300efae82971028b220b135331d94
josh wainwright <wainwright.ja@gmail.com>
2013-02-02 13:41:34 +0000

    Testing webpage

---
+++ javascripts/customscript.js
@@ -0,0 +1,31 @@
+// Toggle visibility of headings
+function toggle_visibility(id) {
+	var e = document.getElementById(id);
+	if(e.style.display == 'block')
+		e.style.display = 'none';
+	else
+		e.style.display = 'block';
+}
+
+function shrink_menu_panel(id){
+	var header = document.getElementById(id);
+	// if(header.style.width == '270px')
+	// 	header.style.width == '70px';
+	// else
+		header.style.width == "70px";
+}
+
+// Row highlight in table
+window.onload=function(){
+	var tfrow = document.getElementById('tfhover').rows.length;
+	var tbRow=[];
+	for (var i=1;i<tfrow;i++) {
+		tbRow[i]=document.getElementById('tfhover').rows[i];
+		tbRow[i].onmouseover = function(){
+			this.style.backgroundColor = '#f3f8aa';
+		};
+		tbRow[i].onmouseout = function() {
+			this.style.backgroundColor = '#ffffff';
+		};
+	}
+};
