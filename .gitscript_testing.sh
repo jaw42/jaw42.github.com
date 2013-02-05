e196586505cbde4ff6f181210a7b24f46a3b5f44
josh wainwright <wainwright.ja@gmail.com>
2013-02-05 21:12:43 +0000

    Auto comment 20130205

--- .gitscript_testing.sh
+++ .gitscript_testing.sh
@@ -1,7 +1,14 @@
 #!/bin/bash 
+DATE=`date +%Y%m%d`
+echo $DATE
 
 git add . -v
-echo -n "Comment: "
+echo -n "Comment [Auto comment "$DATE"]: "
 read comment
+if [ -z "$comment" ]
+then 
+	comment="Auto comment $DATE"
+	echo $comment
+fi
 git commit -a -v -m "$comment"
 git push -v
