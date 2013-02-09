86eb0d38907f0ad0cf1f2510e41f0f14400ea845
josh wainwright <wainwright.ja@gmail.com>
2013-02-09 00:13:16 +0000

    Auto comment 20130209

--- javascripts/customscript.js
+++ javascripts/customscript.js
@@ -20,21 +20,6 @@ function shrink_menu_panel(id){
 		header.style.width == "70px";
 }
 
-// Row highlight in table
-window.onload=function(){
-	var tfrow = document.getElementById('tfhover').rows.length;
-	var tbRow=[];
-	for (var i=1;i<tfrow;i++) {
-		tbRow[i]=document.getElementById('tfhover').rows[i];
-		tbRow[i].onmouseover = function(){
-			this.style.backgroundColor = '#f3f8aa';
-		};
-		tbRow[i].onmouseout = function() {
-			this.style.backgroundColor = '#ffffff';
-		};
-	}
-};
-
 function showOverlay() {
 	var o = document.getElementById('overlay');
 	o.style.visibility = 'visible';
